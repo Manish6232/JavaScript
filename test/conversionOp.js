e75256860514e4fc69d2b56622c0d66b6624cefd
Manish Rajak <152643881+Manish6232@users.noreply.github.com>
2025-01-27 09:47:44 +0000

    Conversion  & Operators

---
+++ test/conversionOp.js
@@ -0,0 +1,67 @@
+let score = "manish"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// conversion :-
+// "33" => 33
+// "33asfd" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "manish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// Boolean conversion :-
+// 1 => true; 0 => false
+// "" => false
+// "kuch be likhte ho" => true
+
+// String conversion :-
+// 33 => 33 in string
+
+
+
+//****************** Operation **************** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%2);
+let str1 = "hello"
+let str2 = "manish"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log((3+4) * 5 % 3); // 2
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
