64cbb3c589027190ab1b4c69d89083a780080603
Manish Rajak <152643881+Manish6232@users.noreply.github.com>
2025-01-27 09:01:54 +0000

    datatypes

---
+++ test/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all  JS code as new version
+
+console.log(3+3)
+
+let name = "manish"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // (typeof "manish") o/p => string output of undefined => undefined
+console.log(typeof null); // null ek object hai
